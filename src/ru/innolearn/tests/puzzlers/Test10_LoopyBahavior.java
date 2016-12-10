4bc5fa42f5cfea02358d7618b3064bd0a9b0e69e
Ifgeny87 <ifgeny87@gmail.com>
2016-12-10 17:56:11 +0300

    Puzzlers init

---
+++ src/ru/innolearn/tests/puzzlers/Test10_LoopyBahavior.java
@@ -0,0 +1,19 @@
+package ru.innolearn.tests.puzzlers;
+
+/**
+ * Created by marina on 09.12.2016.
+ */
+class Test10_LoopyBahavior {
+	public static void main(String[] args) {
+		final int start = Integer.MAX_VALUE - 10;
+		final int end = Integer.MAX_VALUE;
+
+		System.out.printf("%d %d\n", start, end);
+
+		int count = 0;
+		for (int i = start; i <= end && count < 20; i++) {
+			count++;
+			System.out.println(i + ": " + count);
+		}
+	}
+}
