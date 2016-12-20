abf10bd9461c0d797f98e3ab06f96e9a117aed10
Ifgeny87 <ifgeny87@gmail.com>
2016-12-20 10:55:38 +0300

    Перенес сюда задачи по срезам

---
+++ src/main/java/ru/classwork/week01/Variant1.java
@@ -0,0 +1,81 @@
+package ru.classwork.week01;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+/**
+ * Вариант 1:
+ * Реализовать программу из 2-х потоков. Один из потоков каждую секунду генерирует случайное число в интервале [0;99]. Второй поток раз в пять секунд выводит количество уникальных чисел, сгенерированных первым потоком. После того, как будет сгенерировано все 100 чисел, оба потока должны остановить свое выполнение.
+ */
+public class Variant1 {
+	public static void main(String[] args) throws InterruptedException {
+		T1 t1 = new T1();
+		T2 t2 = new T2();
+
+		t1.start();
+		t2.start();
+
+		while(t1.isAlive())
+			synchronized (t1) {
+				t1.wait(10);
+			}
+
+		t2.interrupt();
+	}
+}
+
+/**
+ * Настройки приложения
+ * Для поставленной задачи работает слишком быстро, вывод не появляется
+ */
+class Config {
+	// количество уникальных значений
+	final static int uniqueNumbersCount = 100;
+
+	// периодичность вывода во втором потоке
+	final static int thread2Sleep = 5000;
+}
+
+/**
+ * Класс-хранилище
+ */
+class Counter {
+	public static Set<Integer> numbers = new HashSet<>();
+}
+
+/**
+ * Класс первого потока
+ * Выполняет генерацию и запоминает числа
+ */
+class T1 extends Thread {
+	Random random = new Random();
+
+	@Override
+	public void run() {
+		while(!isInterrupted()) {
+			int n = random.nextInt(Config.uniqueNumbersCount);
+			Counter.numbers.add(n);
+			if(Counter.numbers.size() == Config.uniqueNumbersCount)
+				break;
+		}
+	}
+}
+
+/**
+ * Класс второго потока
+ * Просто выводит текущее состояние работы
+ */
+class T2 extends Thread {
+	@Override
+	public void run() {
+		while(!isInterrupted()) {
+			try {
+				Thread.sleep(Config.thread2Sleep);
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+			}
+			System.out.println(Counter.numbers.size());
+		}
+	}
+}
---
+++ src/main/java/ru/classwork/week01/Variant2.java
@@ -0,0 +1,85 @@
+package ru.classwork.week01;
+
+import java.util.*;
+
+/**
+ * Вариант 2:
+ Реализовать программу из 2-х потоков. Один из потоков каждую секунду генерирует случайное число в интервале [0;99]. Второй поток раз в пять секунд выводит количество раз, которое каждое из чисел было сгенерированно. После того, как какое-либо из чисел будет сгенерированно не менее, чем 5 раз, оба потока должны остановить свое выполнение.
+ */
+public class Variant2 {
+	public static void main(String[] args) throws InterruptedException {
+		WT1 wt1 = new WT1();
+		WT2 wt2 = new WT2();
+
+		wt1.start();
+		wt2.start();
+
+		while(wt1.isAlive())
+			synchronized (wt1) {
+				wt1.wait(10);
+			}
+
+		wt2.interrupt();
+	}
+}
+
+/**
+ * Настройки приложения
+ * Для поставленной задачи работает слишком быстро, вывод не появляется
+ */
+class Config2 {
+	// количество уникальных значений
+	final static int uniqueNumbersCount = 100;
+
+	// ограничение на количество чисел
+	final static int numbersCountMax = 5;
+
+	// периодичность вывода во втором потоке
+	final static int thread2Sleep = 5000;
+}
+
+/**
+ * Класс-хранилище
+ */
+class Summer {
+	public static Map<Integer,Integer> numbers = new HashMap<>();
+}
+
+/**
+ * Класс первого потока
+ * Выполняет генерацию и запоминает числа
+ */
+class WT1 extends Thread {
+	Random random = new Random();
+
+	@Override
+	public void run() {
+		while(!isInterrupted()) {
+			int n = random.nextInt(Config2.uniqueNumbersCount);
+			int count = Summer.numbers.getOrDefault(n, 0);
+			Summer.numbers.put(n, ++count);
+			if(count >= Config2.numbersCountMax)
+				break;
+		}
+	}
+}
+
+/**
+ * Класс второго потока
+ * Просто выводит текущее состояние работы
+ */
+class WT2 extends Thread {
+	@Override
+	public void run() {
+		while(!isInterrupted()) {
+			try {
+				Thread.sleep(Config2.thread2Sleep);
+				Summer.numbers.forEach((n, count) -> {
+					System.out.printf("%d\t%,d\n", n, count);
+				});
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+			}
+		}
+	}
+}
